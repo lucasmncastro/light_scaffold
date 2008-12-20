b4d2cdf870b5a229edce4105d1cb4f139e3789fa
Lucas <castro.lucas@gmail.com>
2008-12-20 18:58:29 -0300

    Added test project.

--- install.rb
+++ install.rb
@@ -1,1 +1,6 @@
-# Install hook code here
+require 'fileutils'
+
+# Copy views dir.
+
+FileUtils.mkdir "#{RAILS_ROOT}/app/views/scaffold"
+FileUtils.cp_r 'views/scaffold', "#{RAILS_ROOT}/app/views/scaffold"
