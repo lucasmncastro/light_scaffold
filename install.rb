6c0db77fc0b84893930e71907b0d4c282ce038ba
Lucas de Castro <castro.lucas@gmail.com>
2009-07-16 18:11:17 -0300

    renaming from resource_view to light_scaffold

--- install.rb
+++ install.rb
@@ -3,5 +3,5 @@ require 'fileutils'
 scaffold_views = File.join %W(#{RAILS_ROOT} app views scaffold)
 FileUtils.mkdir scaffold_views
 
-source = File.join %w(vendor plugins resource_view views scaffold/.)
+source = File.join %w(vendor plugins light_scaffold views scaffold/.)
 FileUtils.cp_r source, scaffold_views
