744f18dd2ae4d2d3bb2ad0e8414d0335de4e5ecd
lucasdecastro <castro.lucas@gmail.com>
2009-09-20 21:39:42 -0300

    PathSet fixed for Rails 2.3

--- lib/light_scaffold/path_set.rb
+++ lib/light_scaffold/path_set.rb
@@ -2,19 +2,20 @@ module LightScaffold
   module PathSet
     def self.included(klass)
       super
-      klass.class_eval { alias_method_chain :find, :shared_views }
+      klass.class_eval { alias_method_chain :find_template, :shared_views }
     end
 
-    def find_with_shared_views(path, details = {}, prefix = nil, partial = false)
-      find_without_shared_views(path, details, prefix, partial)
+    def find_template_with_shared_views(original_template_path, form = nil, html_fallback = true)
+      find_template_without_shared_views(original_template_path, form, html_fallback)
     rescue ActionView::MissingTemplate => original_exception
       begin
-        find_without_shared_views(path, details, shared_views_prefix, partial)    
+        action_name = original_template_path.split('/').last
+        find_template_without_shared_views("#{shared_views_prefix}/#{action_name}", form, html_fallback)
       rescue ActionView::MissingTemplate
         raise original_exception
       end
     end  
-    
+ 
     def shared_views_prefix
       'scaffold'
     end
