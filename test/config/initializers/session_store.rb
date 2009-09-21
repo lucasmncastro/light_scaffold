345f3737178ba2ab7bc80e8a3cd2ee442fc12a18
lucasdecastro <castro.lucas@gmail.com>
2009-09-20 21:38:56 -0300

    Rails 2.3.4 on test app

--- test/config/initializers/session_store.rb
+++ test/config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_test_session',
-  :secret      => 'eadae7bd5dc8ee4981b9d5f1be0df3d980adb04db26663383698d192d0f9d632da5e72589d7626e48dcb1f8162398bae8d29e6422f1bb22e971c1ef63470ff04'
+  :secret      => '02635fb3f96e36597ae0184a3c0dd586af60ca2027c18fb296bfca48214530d9cefdb0e80f80f25009c0e84ac0c02b8c4c17bbd963e220abf76e068eed5cb317'
 }
 
 # Use the database for sessions instead of the cookie-based default,
