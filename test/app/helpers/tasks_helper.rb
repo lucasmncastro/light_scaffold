ce9855d3608b28a12e1ea429887a5d0b1afcba9f
lucasdecastro <castro.lucas@gmail.com>
2009-09-24 18:32:49 -0300

    Renaming helper methods for form fields

--- test/app/helpers/tasks_helper.rb
+++ test/app/helpers/tasks_helper.rb
@@ -18,11 +18,11 @@ module TasksHelper
     [:description, :status, :magic]
   end
 
-  def status_field(form, resource)
+  def status_form_field(form, resource)
     form.check_box :status
   end
   
-  def description_field(form, resource)
+  def description_form_field(form, resource)
     form.text_area :description
   end
 
